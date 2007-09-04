74556d96e7971524f774396241073d4741f94884
(no author) <(no author)@cd1c1023-2f26-0410-ae45-c471fc1f0318>
2007-09-04 07:53:06 +0000

    update handlers
    git-svn-id: http://svn.sabayonlinux.org/projects/entropy/trunk@480 cd1c1023-2f26-0410-ae45-c471fc1f0318

--- handlers/error_report.php
+++ handlers/error_report.php
@@ -2,10 +2,13 @@
 $header  = "MIME-Version: 1.0\r\n";
 $header .= "Content-type: text/html; charset=iso-8859-1\r\n";
 $arch = $_GET['arch'];
-$mail = "lxnay@sabayonlinux.org";
+$mail = "fabio.erculiani@tiscali.it";
 $subject = "Entropy Error Reporting Handler";
 $message = '<html><head><title>Entropy Error Reporting Handler</title></head><body><p>';
-$message .= $_GET['stacktrace'] . "<br><br><br> Architecture: " . $arch . "</p></body></html>";
-print_r($message);
-mail($mail,$subject,$message, $header);
+$message .= $_GET['stacktrace'] . "<br><br><br> Architecture: " . $arch . "<br>";
+$message .= 'IP: ' . $_SERVER['REMOTE_ADDR'] . '<br>'; 
+$message .= 'Date: ' . date("G:i d/F/Y") . '<br>'; 
+$message .= "</p></body></html>";
+if ($_GET['stacktrace'] && $_GET['arch'])
+	mail($mail,$subject,$message, $header);
 ?>
--- handlers/md5sum.php
+++ handlers/md5sum.php
@@ -1,26 +1,28 @@
 <?php
 
-	function insert_package($pkgfile,$pkgpath,$db) {
+	function insert_package($pkgpath,$db) {
 		$mtime = filemtime($pkgpath);
 		$md5 = md5_file($pkgpath);
-		$sql = "insert into checksums (filename,mtime,md5) values ('".$pkgfile."', '".$mtime."', '".$md5."')";
+		$sql = "insert into checksums (filename,mtime,md5) values ('".$pkgpath."', '".$mtime."', '".$md5."')";
 		sqlite_query($db, $sql);
 		return $md5;
 	}
 
-	function delete_package($pkgfile,$db) {
-		$sql = "delete from checksums where filename ='".$pkgfile."';";
+	function delete_package($pkgpath,$db) {
+		$sql = "delete from checksums where filename ='".$pkgpath."';";
 		sqlite_query($db, $sql);
 	}
 
 	$pkgfile = urlencode($_GET['package']);
 	$pkgarch = $_GET['arch'];
-	$pkgpath = "/home/fabio/entropy/packages/" . $pkgarch . "/" . $pkgfile;
+	$pkgpath = "../packages/" . $pkgarch . "/" . $pkgfile;
 
-	if (!file_exists($pkgpath))
+	if (!file_exists($pkgpath)) {
 		print("-1");
+		return;
+	}
 
-	$dbfile = "/home/fabio/public_html/svn.sabayonlinux.org/entropy/handlers/checksums.db";
+	$dbfile = "checksums.db";
 	if (file_exists($dbfile)) {
 		$db = sqlite_open($dbfile);
 	} else {
@@ -30,12 +32,12 @@
 
 	$mtime = filemtime($pkgpath);
 
-	$sql = "select * from checksums where filename ='".$pkgfile."';";
+	$sql = "select * from checksums where filename ='".$pkgpath."';";
 	$result = sqlite_query($db, $sql);
 	$row = sqlite_fetch_array($result);
 
 	if(!$row) {
-		$md5 = insert_package($pkgfile,$pkgpath,$db);
+		$md5 = insert_package($pkgpath,$db);
 		print($md5);
 	} else {
 		// check if mtime is valid
@@ -43,8 +45,8 @@
 		if ($dbmtime == $mtime) {
 			print($row['md5']);
 		} else {
-			delete_package($pkgfile,$db);
-			$md5 = insert_package($pkgfile,$pkgpath,$db);
+			delete_package($pkgpath,$db);
+			$md5 = insert_package($pkgpath,$db);
 			print($md5);
 		}
 	}
