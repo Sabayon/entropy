7705613532c48d916c4084a884dec2e2bf8b2d9c
(no author) <(no author)@cd1c1023-2f26-0410-ae45-c471fc1f0318>
2007-11-23 15:09:13 +0000

    - updated TOOD - added listAllCategories db function
    git-svn-id: http://svn.sabayonlinux.org/projects/entropy/trunk@759 cd1c1023-2f26-0410-ae45-c471fc1f0318

--- libraries/databaseTools.py
+++ libraries/databaseTools.py
@@ -2576,6 +2576,11 @@ class etpDatabase:
 	else:
 	    return self.fetchall2list(self.cursor.fetchall())
 
+    def listAllCategories(self):
+	dbLog.log(ETP_LOGPRI_INFO,ETP_LOGLEVEL_VERBOSE,"listAllCategories: called.")
+	self.cursor.execute('SELECT categories.category FROM baseinfo,categories WHERE baseinfo.idcategory = categories.idcategory')
+	return self.fetchall2set(self.cursor.fetchall())
+
     def listConfigProtectDirectories(self, mask = False):
 	dbLog.log(ETP_LOGPRI_INFO,ETP_LOGLEVEL_VERBOSE,"listConfigProtectDirectories: called.")
 	dirs = set()
