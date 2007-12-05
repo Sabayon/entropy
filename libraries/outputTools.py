0c19d61593e559969402f864edd429fd40d03dd6
(no author) <(no author)@cd1c1023-2f26-0410-ae45-c471fc1f0318>
2007-12-05 12:04:51 +0000

    testing new line clean method
    git-svn-id: http://svn.sabayonlinux.org/projects/entropy/trunk@799 cd1c1023-2f26-0410-ae45-c471fc1f0318

--- libraries/outputTools.py
+++ libraries/outputTools.py
@@ -235,7 +235,7 @@ def print_error(msg, back = False):
 	    _lastline = msg
         else:
 	    return
-	writechar("\r"+_cleanline+"\r")
+	writechar("\r"+_lastline.rstrip()+"\r")
 	writechar("\r"+red(">>")+" "+msg)
 	return
     writechar("\r"+_cleanline+"\r")
@@ -248,7 +248,8 @@ def print_info(msg, back = False):
 	    _lastline = msg
         else:
 	    return
-	writechar("\r"+_cleanline+"\r")
+        
+	writechar("\r"+_lastline.rstrip()+"\r")
 	writechar("\r"+green(">>")+" "+msg)
 	return
     writechar("\r"+_cleanline+"\r")
