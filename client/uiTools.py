ecd6eaa30e5e58409289c3601f94ff32bf2f0e20
(no author) <(no author)@cd1c1023-2f26-0410-ae45-c471fc1f0318>
2007-12-16 16:17:09 +0000

    fixed a typo
    git-svn-id: http://svn.sabayonlinux.org/projects/entropy/trunk@892 cd1c1023-2f26-0410-ae45-c471fc1f0318

--- client/uiTools.py
+++ client/uiTools.py
@@ -617,7 +617,7 @@ def installPackages(packages = [], atomsdata = [], deps = True, emptydeps = Fals
             # filter out packages that are in actionQueue comparing key + slot
             if runQueue:
                 myremmatch = {}
-                [myremmatch[(entropyTools.dep_getkey(clientDbconn.retrieveAtom(x)),clientDbconn.retrieveSlot(x))] = x for x in removalQueue]
+                [myremmatch.update({(entropyTools.dep_getkey(clientDbconn.retrieveAtom(x)),clientDbconn.retrieveSlot(x)): x}) for x in removalQueue]
                 for packageInfo in runQueue:
                     dbconn = openRepositoryDatabase(packageInfo[1])
                     testtuple = (entropyTools.dep_getkey(dbconn.retrieveAtom(packageInfo[0])),dbconn.retrieveSlot(packageInfo[0]))
