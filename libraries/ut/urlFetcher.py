20d5f806bfcef4a998286185f603dba6752473ab
lxnay <lxnay@cd1c1023-2f26-0410-ae45-c471fc1f0318>
2009-02-19 14:18:42 +0000

    Entropy: - add unit test dir
    git-svn-id: http://svn.sabayonlinux.org/projects/entropy/trunk@3044 cd1c1023-2f26-0410-ae45-c471fc1f0318

---
+++ libraries/ut/urlFetcher.py
@@ -0,0 +1,5 @@
+from entropy import urlFetcher
+url = "http://svn.sabayonlinux.org/entropy/standard/sabayonlinux.org/database/amd64/4/packages.db.bz2"
+to = "/tmp/packages.db.bz2"
+u = urlFetcher(url, to)
+print u.download()
