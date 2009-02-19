4865a100b85189a82ca5df2af36ad2900c22d2f0
lxnay <lxnay@cd1c1023-2f26-0410-ae45-c471fc1f0318>
2009-02-19 16:15:20 +0000

    Entropy/UT: - fix urlFetcher test
    git-svn-id: http://svn.sabayonlinux.org/projects/entropy/trunk@3045 cd1c1023-2f26-0410-ae45-c471fc1f0318

--- libraries/ut/urlFetcher.py
+++ libraries/ut/urlFetcher.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.insert(0,'../')
 from entropy import urlFetcher
 url = "http://svn.sabayonlinux.org/entropy/standard/sabayonlinux.org/database/amd64/4/packages.db.bz2"
 to = "/tmp/packages.db.bz2"
