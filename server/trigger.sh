5b19ab1f06400a95bf3a84bb9e236ce10e3c125f
lxnay <lxnay@cd1c1023-2f26-0410-ae45-c471fc1f0318>
2009-02-27 17:46:50 +0000

    Entropy/Bash Trigger Specification: - add documentation about exported env variables
    git-svn-id: http://svn.sabayonlinux.org/projects/entropy/trunk@3077 cd1c1023-2f26-0410-ae45-c471fc1f0318

--- server/trigger.sh
+++ server/trigger.sh
@@ -1,4 +1,35 @@
 #!/usr/sbin/entropy.sh
+# Environmental variables available
+#
+# always available:
+#
+# ETP_API: Current Entropy client API
+# ETP_LOG: Entropy log file path
+# ETP_STAGE: Entropy trigger stage (preinstall,postinstall,preremove,postremove)
+# ETP_PHASE: Entropy trigger phase, matching Portage ones
+#                    (pkg_preinst, pkg_prerm, pkg_preinst, pkg_postinst)
+# ETP_BRANCH: Current Entropy client branch
+# CATEGORY: Entropy package category
+# PN: Entropy package name
+# PV: Entropy package version
+# PR: Entropy package revision
+# PVR: Entropy package version+revision
+# PVRTE: Entropy package version+revision+entropy_tag+entropy_rev
+# PER: Entropy package revision
+# PET: Entropy package tag
+# SLOT: Entropy package slot
+# PAPI: Entropy package "Entropy API"
+# P: Entropy package atom
+# CFLAGS: Entropy package CFLAGS
+# CXXFLAGS: Entropy package CXXFLAGS
+# CHOST: Entropy package CHOST
+# PORTAGE_ECLASSES: Portage package used eclasses
+# WORKDIR: Entropy temporary package unpack/work dir (matching Portage)
+# B: Entropy temporary package unpack/work dir
+# D: Entropy temporary final package destination dir (before merging to system)
+# ENTROPY_TMPDIR: Entropy packages temporary directory
+# ROOT: System root directory, "" if /
+#
 
 function pkg_preinst() {
     echo "preinst"
