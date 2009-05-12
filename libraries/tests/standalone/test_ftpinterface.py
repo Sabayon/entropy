55e96d0050a8b2f29382dc999a6f9bfd4db5885a
Fabio Erculiani <lxnay@sabayonlinux.org>
2009-05-12 16:58:47 +0200

    unit testing: add some misc test scripts

---
+++ libraries/tests/standalone/test_ftpinterface.py
@@ -0,0 +1,7 @@
+from entropy.transceivers import FtpInterface
+from entropy.output import TextInterface
+text = TextInterface()
+
+ftp = FtpInterface("ftp://", text)
+print ftp.get_file_md5("test.rnd")
+ftp.close()
---
+++ libraries/tests/standalone/test_ugc_update.py
@@ -0,0 +1,5 @@
+from entropy.client.interfaces import Client
+cl = Client()
+print cl.UGC.get_all_downloads("sabayonlinux.org")
+cl.destroy()
+raise SystemExit(0)
