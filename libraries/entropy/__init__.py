4db9520568492ad5b8b812483d70030d519b84c7
Fabio Erculiani <lxnay@sabayonlinux.org>
2009-05-20 19:20:32 +0200

    clear entropy/__init__.py

--- libraries/entropy/__init__.py
+++ libraries/entropy/__init__.py
@@ -20,30 +20,3 @@
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 '''
 
-# Old critical imports kept for compatibility
-# WARNING WARNING WARNING WARNING WARNING WARNING WARNING
-# !!! Will be removed soon !!!
-# WARNING WARNING WARNING WARNING WARNING WARNING WARNING
-# from entropy.client.interfaces import Client as EquoInterface, Package as PackageInterface, Repository as RepoInterface
-# from entropy.db import LocalRepository as EntropyDatabaseInterface, dbapi2
-# from entropy.spm import Spm as SpmInterface
-# from entropy.client.misc import FileUpdates
-# from entropy.services.interfaces import SocketHost as SocketHostInterface
-# from entropy.services.skel import SocketCommands as SocketCommandsSkel, \
-#    SocketAuthenticator as SocketAuthenticatorSkel, RemoteDatabase as RemoteDbSkelInterface, \
-#    Authenticator as DistributionAuthInterface
-# from entropy.server.interfaces import Server as ServerInterface, MirrorsServer as ServerMirrorsInterface
-# from entropy.services.ugc.interfaces import Server as DistributionUGCInterface, Client as SystemSocketClientInterface
-# from entropy.client.services.ugc.interfaces import Client as UGCClientInterface, AuthStore as UGCClientAuthStore, Cache as UGCCacheInterface
-# from entropy.client.services.ugc.commands import Base as EntropySocketClientCommands, Client as RepositorySocketClientCommands
-# from entropy.client.services.system.commands import Client as SystemManagerClientCommands, Repository as SystemManagerRepositoryClientCommands
-# from entropy.client.services.system.interfaces import Client as SystemManagerClientInterface
-# from entropy.client.services.system.methods import Base as SystemManagerMethodsInterface, Repository as SystemManagerRepositoryMethodsInterface
-# from entropy.services.ugc.commands import UGC as DistributionUGCCommands
-# from entropy.services.auth_interfaces import phpBB3Auth as phpBB3AuthInterface
-# from entropy.services.authenticators import phpBB3 as phpbb3Authenticator
-# from entropy.services.commands import phpBB3 as phpbb3Commands
-# from entropy.services.repository.interfaces import Server as RepositorySocketServerInterface
-# from entropy.services.system.commands import Repository as SystemManagerRepositoryCommands
-# from entropy.services.system.interfaces import TaskExecutor as SystemManagerExecutorInterface, Server as SystemManagerServerInterface
-# from entropy.services.system.executors import Base as SystemManagerExecutorServerRepositoryInterface
