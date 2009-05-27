67e220e81e22552438a78f39232d3cba3fd35cf6
Fabio Erculiani <lxnay@sabayonlinux.org>
2009-05-27 20:57:56 +0200

    add Entropy hardware has generator

---
+++ misc/entropy_hwgen.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Entropy Hardware hash generator
+
+[[ -x "/sbin/ifconfig" ]] || exit 1
+[[ -x "/usr/sbin/lspci" ]] || exit 1
+[[ -x "/usr/bin/sha256sum" ]] || exit 1
+
+ifconfig_output=$(/sbin/ifconfig -a | grep HWaddr 2> /dev/null)
+lspci_output=$(/usr/sbin/lspci -n | cut -d" " -f 3- 2> /dev/null)
+echo $ifconfig_output$lspci_output | /usr/bin/sha256sum | cut -d" " -f 1
