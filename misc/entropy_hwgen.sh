74f472d2e54f04213d4daf89a6a26257cf4386b1
Fabio Erculiani <lxnay@sabayonlinux.org>
2009-05-28 06:43:51 +0200

    improve entropy hardware hash generator

--- misc/entropy_hwgen.sh
+++ misc/entropy_hwgen.sh
@@ -7,4 +7,6 @@
 
 ifconfig_output=$(/sbin/ifconfig -a | grep HWaddr 2> /dev/null)
 lspci_output=$(/usr/sbin/lspci -n | cut -d" " -f 3- 2> /dev/null)
-echo $ifconfig_output$lspci_output | /usr/bin/sha256sum | cut -d" " -f 1
+random_seed=$(echo $RANDOM$RANDOM$RANDOM)
+echo $ifconfig_output$lspci_output$random_seed | /usr/bin/sha256sum | \
+    cut -d" " -f 1
