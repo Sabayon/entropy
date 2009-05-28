8ff39fe5cd11bbb9158fab8c0ba1fb7bf2436113
Fabio Erculiani <lxnay@sabayonlinux.org>
2009-05-28 13:21:15 +0200

    fine tune entropy hardware hash generator

--- misc/entropy_hwgen.sh
+++ misc/entropy_hwgen.sh
@@ -2,11 +2,10 @@
 # Entropy Hardware hash generator
 
 [[ -x "/sbin/ifconfig" ]] || exit 1
+[[ -x "/usr/bin/awk" ]] || exit 1
 [[ -x "/usr/sbin/lspci" ]] || exit 1
 [[ -x "/usr/bin/sha256sum" ]] || exit 1
 
-ifconfig_output=$(/sbin/ifconfig -a | grep HWaddr 2> /dev/null)
+ifconfig_output=$(/sbin/ifconfig -a | grep HWaddr | awk '{ print $5 }' 2> /dev/null)
 lspci_output=$(/usr/sbin/lspci -n | cut -d" " -f 3- 2> /dev/null)
-random_seed=$(echo $RANDOM$RANDOM$RANDOM)
-echo $ifconfig_output$lspci_output$random_seed | /usr/bin/sha256sum | \
-    cut -d" " -f 1
+echo $ifconfig_output$lspci_output | /usr/bin/sha256sum | cut -d" " -f 1
