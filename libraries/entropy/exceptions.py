df6952f91386135eb36132e2076392721508ae27
Fabio Erculiani <lxnay@sabayonlinux.org>
2009-07-01 14:42:07 +0200

    [entropy.exceptions] complete docstrings

--- libraries/entropy/exceptions.py
+++ libraries/entropy/exceptions.py
@@ -1,28 +1,16 @@
-'''
-    # DESCRIPTION:
-    # Entropy exceptions class
+# -*- coding: utf-8 -*-
+"""
 
-    Copyright (C) 2007-2009 Fabio Erculiani
+    @author: Fabio Erculiani <lxnay@sabayonlinux.org>
+    @contact: lxnay@sabayonlinux.org
+    @copyright: Fabio Erculiani
+    @license: GPL-2
 
-    structure inspired from portage_exception.py
-    Copyright 1998-2004 Gentoo Foundation
-    # $Id: portage_exception.py 6885 2007-06-20 05:45:31Z zmedico $
+    B{Entropy Framework exceptions class module}
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+    This module contains Entropy Framework exceptions classes.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-'''
-# pylint ok
+"""
 
 class EntropyException(Exception):
     """General superclass for Entropy exceptions"""
