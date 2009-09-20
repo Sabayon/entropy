8862b3619f361b95478fbd506d99e74ac3a7cd8a
Fabio Erculiani <lxnay@sabayonlinux.org>
2009-09-20 21:40:40 +0200

    [magneto] KDE: close notification on timeout

--- magneto/src/magneto/kde/interfaces.py
+++ magneto/src/magneto/kde/interfaces.py
@@ -157,7 +157,8 @@ class Magneto(MagnetoCore):
             return
 
         def do_show():
-            notification = KNotification("Updates")
+            notification = KNotification("Updates",
+                flags = KNotification.CloseOnTimeOut)
             notification.setText("<b>%s</b><br/>%s" % (title, text,))
             if urgency == 'critical':
                 notification.setPixmap(self.icons.best_match("critical",22))
