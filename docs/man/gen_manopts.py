dee75d808a51d3edb28334d2d2396aea2eb96392
Fabio Erculiani <lxnay@sabayonlinux.org>
2009-10-04 18:29:55 +0200

    [docs] make script Python 3.x compatible

--- docs/man/gen_manopts.py
+++ docs/man/gen_manopts.py
@@ -1,4 +1,6 @@
-#!/usr/bin/python2
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import sys
 
 def _(x):
     return x
@@ -8,12 +10,13 @@ myopts = []
 old_indent_level = 1
 for indent_level, name, space_level, desc in myopts:
     if indent_level > old_indent_level:
-        print("\n=over\n"*(indent_level-old_indent_level))
+        sys.stdout.write("\n=over\n"*(indent_level-old_indent_level) + "\n")
         old_indent_level = indent_level
     elif indent_level < old_indent_level:
-        print("\n=back\n"*(old_indent_level-indent_level))
+        sys.stdout.write("\n=back\n"*(old_indent_level-indent_level) + "\n")
         old_indent_level = indent_level
-    print("""
+    sys.stdout.write("""
 =item B<%s>
 
-%s"""     % (name, desc,))
+%s
+""" % (name, desc,))
