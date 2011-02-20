65f741078dfe1a37750c787fda58bc7ce0301d11
Fabio Erculiani <lxnay@sabayon.org>
2011-02-20 19:43:24 +0100

    [tests+ add hardlink test

---
+++ libraries/tests/standalone/test_hardlinks.py
@@ -0,0 +1,30 @@
+import os, errno
+from entropy.const import etpUi
+etpUi['quiet'] = True
+from entropy.server.interfaces import Server
+
+srv = Server()
+repos = srv.repositories()
+atom_cache = set()
+
+for repo_id in repos:
+    repo = srv.open_repository(repo_id)
+    pkg_ids = repo.listAllPackageIds(order_by="atom")
+    for pkg_id in pkg_ids:
+        atom = repo.retrieveAtom(pkg_id)
+        if atom in atom_cache:
+            continue
+        content = repo.retrieveContent(pkg_id)
+        for path in content:
+            try:
+                st = os.lstat(path)
+            except OSError as err:
+                if err.errno != errno.ENOENT:
+                    raise
+            if st.st_nlink > 1:
+                # hard link !
+                print(atom)
+                atom_cache.add(atom)
+                break
+
+srv.shutdown()
