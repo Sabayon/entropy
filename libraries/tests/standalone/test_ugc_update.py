b930bef29d54f6bfdaba43537d76cb2f66795840
Fabio Erculiani <lxnay@sabayon.org>
2011-04-10 23:15:51 +0200

    [tests] drop dead tests

--- libraries/tests/standalone/test_ugc_update.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from entropy.output import print_generic
-from entropy.client.interfaces import Client
-cl = Client()
-print_generic(cl.UGC.get_all_downloads("sabayonlinux.org"))
-cl.destroy()
-raise SystemExit(0)
