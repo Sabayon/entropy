2e00418c0ea3fd8e1da3936c4ec252c3813fa8d1
Fabio Erculiani <lxnay@sabayon.org>
2011-08-01 14:42:16 +0200

    [matter] complete initial implementation

--- services/auto-builder_examples/post.sh
+++ services/auto-builder_examples/post.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 echo "matter post hook"
 echo "BUILDER_REPOSITORY_ID = ${BUILDER_REPOSITORY_ID}"
-echo "BUILDER_CHROOT_DIR = ${BUILDER_CHROOT_DIR}"
--- services/auto-builder_examples/pre.sh
+++ services/auto-builder_examples/pre.sh
@@ -1,17 +1,9 @@
 #!/bin/sh
 echo "matter pre hook"
 echo "BUILDER_REPOSITORY_ID = ${BUILDER_REPOSITORY_ID}"
-echo "BUILDER_CHROOT_DIR = ${BUILDER_CHROOT_DIR}"
-real_builder_chroot_dir=$(realpath "${BUILDER_CHROOT_DIR}")
 
-is_mounted=$(mount | cut -d" " -f 3 | grep "${real_builder_chroot_dir}/proc")
+is_mounted=$(mount | cut -d" " -f 3 | grep "/proc")
 if [ -z "${is_mounted}" ]; then
 	echo "mounting /proc, not mounted"
-	mount --bind /proc "${real_builder_chroot_dir}"/proc
-fi
-
-is_mounted=$(mount | cut -d" " -f 3 | grep "${real_builder_chroot_dir}/dev")
-if [ -z "${is_mounted}" ]; then
-	echo "mounting /dev, not mounted"
-	mount --bind /dev "${real_builder_chroot_dir}"/dev
+	mount -t proc proc /proc
 fi
