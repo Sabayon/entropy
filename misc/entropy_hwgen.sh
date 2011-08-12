37f6ad16b33ad239fff37efc7d55ecd6cecdd07d
Sławomir Nizio <slawomir.nizio@sabayon.org>
2011-08-13 00:30:06 +0200

    [misc] don't use non-POSIX command/keyword

--- misc/entropy_hwgen.sh
+++ misc/entropy_hwgen.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # Entropy Hardware hash generator
 
-[[ -x "/sbin/ifconfig" ]] || exit 1
-[[ -x "/usr/bin/awk" ]] || exit 1
-[[ -x "/usr/sbin/lspci" ]] || exit 1
-[[ -x "/usr/bin/sha256sum" ]] || exit 1
+[ -x "/sbin/ifconfig" ] || exit 1
+[ -x "/usr/bin/awk" ] || exit 1
+[ -x "/usr/sbin/lspci" ] || exit 1
+[ -x "/usr/bin/sha256sum" ] || exit 1
 
 ifconfig_output=$(/sbin/ifconfig -a | grep HWaddr | awk '{ print $5 }' 2> /dev/null)
 lspci_output=$(/usr/sbin/lspci -n | cut -d" " -f 3- 2> /dev/null)
