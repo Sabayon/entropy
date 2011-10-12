ef00dc6d9426e1d2e45c29fea582881b2e53da76
Fabio Erculiani <lxnay@sabayon.org>
2011-10-12 14:46:03 +0200

    [eit.commands] automatically import all the available submodules

--- server/eit/commands/__init__.py
+++ server/eit/commands/__init__.py
@@ -9,9 +9,19 @@
     B{Entropy Infrastructure Toolkit}.
 
 """
-# TODO: use automatic loader
-from eit.commands.help import EitHelp
-from eit.commands.status import EitStatus
-from eit.commands.commit import EitCommit
-from eit.commands.add import EitAdd
-from eit.commands.repack import EitRepack
+import os
+import sys
+
+_cur_file = sys.modules[__name__].__file__
+_cur_dir = os.path.dirname(_cur_file)
+_excluded_mods = ["descriptor"]
+for py_file in os.listdir(_cur_dir):
+    if not py_file.endswith(".py"):
+        continue
+    if py_file.startswith("_"):
+        continue
+    # strip .py
+    _mod = "eit.commands." + py_file[:-3]
+    if _mod in _excluded_mods:
+        continue
+    __import__(_mod)
