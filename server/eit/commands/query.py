e66f64269ef778badd1593b65e34ff63ab84d403
Fabio Erculiani <lxnay@sabayon.org>
2011-10-16 18:01:26 +0200

    [eit.commands.query] do not lock entropy resources when running

--- server/eit/commands/query.py
+++ server/eit/commands/query.py
@@ -140,7 +140,7 @@ class EitQuery(EitCommand):
         self._quiet = nsargs.quiet
         self._verbose = getattr(nsargs, "verbose", self._verbose)
         self._nsargs = nsargs
-        return self._call_locked, [nsargs.func, self._repository_id]
+        return self._call_unlocked, [nsargs.func, self._repository_id]
 
     def _tags(self, entropy_server):
         """
