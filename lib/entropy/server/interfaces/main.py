a700962ff4e62ccf025155ceb0b845bbbf4dbd26
Fabio Erculiani <lxnay@sabayon.org>
2011-10-30 19:33:38 +0100

    [entropy.server] always makedirs() with 0o775 (gid=entropy)

--- lib/entropy/server/interfaces/main.py
+++ lib/entropy/server/interfaces/main.py
@@ -5637,13 +5637,13 @@ class Server(Client):
         db_dir = self._get_local_repository_dir(repo)
         for mydir in [upload_dir, db_dir]:
             if (not os.path.isdir(mydir)) and (not os.path.lexists(mydir)):
-                os.makedirs(mydir, 0o755)
+                os.makedirs(mydir, 0o775)
                 const_setup_perms(mydir, etpConst['entropygid'],
                     recursion = False, uid = etpConst['uid'])
 
     def _ensure_dir_path(self, dir_path):
         if not os.path.isdir(dir_path):
-            os.makedirs(dir_path, 0o755)
+            os.makedirs(dir_path, 0o775)
             const_setup_perms(dir_path, etpConst['entropygid'],
                 recursion = False, uid = etpConst['uid'])
 
