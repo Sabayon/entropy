7fb23c224ffc0e6af83ea1810f7d0d29c2c491f7
Fabio Erculiani <lxnay@sabayon.org>
2012-03-09 17:29:55 +0100

    [rigo-daemon] first commit, introduce RigoDaemon dbus service

---
+++ rigo/daemon/devel-install-daemon.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ "$(whoami)" != "root" ]; then
+	echo "run this as root" >&2
+	exit 1
+fi
+
+cp dbus/org.sabayon.Rigo.conf /etc/dbus-1/system.d/ || exit 1
+chown root:root /etc/dbus-1/system.d/org.sabayon.Rigo.conf || exit 1
+
+cp dbus/org.sabayon.Rigo.xml /usr/share/dbus-1/interfaces/ || exit 1
+chown root:root /usr/share/dbus-1/interfaces/org.sabayon.Rigo.xml || exit 1
+
+cp dbus/org.sabayon.Rigo.service /usr/share/dbus-1/system-services/ || exit 1
+chown root:root /usr/share/dbus-1/system-services/org.sabayon.Rigo.service || exit 1
+
+sed -i "s:rigo-daemon:rigo-daemon --daemon-logging-disabled --debug:" \
+	/usr/share/dbus-1/system-services/org.sabayon.Rigo.service || exit 1
+
+cp rigo-daemon /usr/sbin/ -p || exit 1
+chmod 755 /usr/sbin/rigo-daemon || exit 1
