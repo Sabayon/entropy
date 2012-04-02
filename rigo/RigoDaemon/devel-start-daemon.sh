e0a9a6c0d687509595b683130737b66232c2eda0
Fabio Erculiani <lxnay@sabayon.org>
2012-04-02 21:20:04 +0200

    [RigoDaemon] devel-start-daemon.sh: add instructions to run app inside gdb

--- rigo/RigoDaemon/devel-start-daemon.sh
+++ rigo/RigoDaemon/devel-start-daemon.sh
@@ -13,4 +13,7 @@ sudo chown root:root /usr/share/dbus-1/interfaces/org.sabayon.Rigo.xml || exit 1
 
 sudo cp polkit/org.sabayon.RigoDaemon.policy /usr/share/polkit-1/actions/ || exit 1
 
-cd ../ && sudo python2 ./RigoDaemon/app.py --debug
+cd ../ || exit 1
+# GDB?
+# sudo gdb --args python2 ./RigoDaemon/app.py --debug
+sudo python2 ./RigoDaemon/app.py --debug
