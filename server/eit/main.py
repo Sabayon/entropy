2e6b176c833f91a3003ae787bb9085285affc17d
Fabio Erculiani <lxnay@sabayon.org>
2012-08-19 00:25:31 +0200

    [eit] do not raise any exception in sys.excepthook

--- server/eit/main.py
+++ server/eit/main.py
@@ -32,8 +32,6 @@ def handle_exception(exc_class, exc_instance, exc_tb):
     # always slap exception data (including stack content)
     entropy.tools.print_exception(tb_data = exc_tb)
 
-    raise exc_instance
-
 def install_exception_handler():
     sys.excepthook = handle_exception
 
