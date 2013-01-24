d4545d0da8d834a9efed7779ecb5bd4515164089
Fabio Erculiani <lxnay@sabayon.org>
2013-01-24 00:16:59 +0000

    [Rigo] use ETP_DEBUG=1 in debug_rigo.sh

--- rigo/debug_rigo.sh
+++ rigo/debug_rigo.sh
@@ -3,4 +3,5 @@ export RIGO_DATA_DIR="./data"
 export RIGO_ICON_PATH="./data/icons"
 # GDB?
 # exec gdb --args python rigo_app.py # --dumper --debug
-exec python rigo_app.py --dumper --debug
+export ETP_DEBUG=1
+exec python rigo_app.py --dumper
