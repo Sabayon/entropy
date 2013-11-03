747cfc9ad4383078d96410c13cd29356bbfd7625
Fabio Erculiani <lxnay@sabayon.org>
2013-11-03 13:16:56 +0100

    [Rigo] debug_rigo.sh: append argv at the end of exec()

--- rigo/debug_rigo.sh
+++ rigo/debug_rigo.sh
@@ -4,4 +4,4 @@ export RIGO_ICON_PATH="./data/icons"
 # GDB?
 # exec gdb --args python rigo_app.py # --dumper --debug
 export ETP_DEBUG=1
-exec python rigo_app.py --dumper
+exec python rigo_app.py --dumper "${@}"
