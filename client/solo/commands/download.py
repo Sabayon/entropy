008b65d11b8b5ab264d02f194a27e4756bc798cf
Fabio Erculiani <lxnay@sabayon.org>
2013-12-06 11:34:33 +0100

    [solo.commands.download] add concurrency support

--- client/solo/commands/download.py
+++ client/solo/commands/download.py
@@ -153,18 +153,22 @@ Download packages, essentially.
         bdeps = self._nsargs.bdeps
         multifetch = self._nsargs.multifetch
 
-        packages = self._scan_packages(
-            entropy_client, self._nsargs.packages)
-        if not packages:
-            entropy_client.output(
-                "%s." % (
-                    darkred(_("No packages found")),),
-                level="error", importance=1)
-            return 1
+        inst_repo = entropy_client.installed_repository()
+        with inst_repo.shared():
 
-        run_queue, removal_queue = self._generate_install_queue(
-            entropy_client, packages, deps, False, deep, relaxed,
-            onlydeps, bdeps, recursive)
+            packages = self._scan_packages(
+                entropy_client, self._nsargs.packages)
+            if not packages:
+                entropy_client.output(
+                    "%s." % (
+                        darkred(_("No packages found")),),
+                    level="error", importance=1)
+                return 1
+
+            run_queue, removal_queue = self._generate_install_queue(
+                entropy_client, packages, deps, False, deep, relaxed,
+                onlydeps, bdeps, recursive)
+
         if (run_queue is None) or (removal_queue is None):
             return 1
         elif not (run_queue or removal_queue):
