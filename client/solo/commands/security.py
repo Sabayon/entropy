19439db61cd5ce873499098f53f6e04c0031c7f4
Fabio Erculiani <lxnay@sabayon.org>
2013-12-06 12:35:07 +0100

    [solo.commands.security] add concurrency support

--- client/solo/commands/security.py
+++ client/solo/commands/security.py
@@ -19,6 +19,7 @@ from entropy.output import darkgreen, darkred, brown, blue, red, \
 
 import entropy.tools
 
+from solo.commands.command import sharedlock
 from solo.commands.descriptor import SoloCommandDescriptor
 from solo.commands.install import SoloInstall
 from solo.utils import print_table
@@ -195,7 +196,7 @@ System security tools.
             return parser.print_help, []
 
         self._nsargs = nsargs
-        return self._call_locked, [nsargs.func]
+        return self._call_unlocked, [nsargs.func]
 
     def bashcomp(self, last_arg):
         """
@@ -334,27 +335,11 @@ System security tools.
 
         print_table(entropy_client, toc, cell_spacing=3)
 
-    def _oscheck(self, entropy_client):
+    def _oscheck_scan_unlocked(self, entropy_client, inst_repo,
+                               quiet, verbose, assimilate):
         """
-        Solo Security Oscheck command.
+        Execute the filesystem scan.
         """
-        mtime = self._nsargs.mtime
-        assimilate = self._nsargs.assimilate
-        reinstall = self._nsargs.reinstall
-        verbose = self._nsargs.verbose
-        quiet = self._nsargs.quiet
-        ask = self._nsargs.ask
-        pretend = self._nsargs.pretend
-        fetch = self._nsargs.fetch
-
-        inst_repo = entropy_client.installed_repository()
-
-        if not quiet:
-            entropy_client.output(
-                "%s..." % (
-                    blue(_("Checking system files")),),
-                header=darkred(" @@ "))
-
         pkg_ids = inst_repo.listAllPackageIds()
         total = len(pkg_ids)
         faulty_pkg_ids = []
@@ -463,6 +448,32 @@ System security tools.
                         )
                         entropy_client.output(txt)
 
+        return faulty_pkg_ids
+
+    def _oscheck(self, entropy_client):
+        """
+        Solo Security Oscheck command.
+        """
+        mtime = self._nsargs.mtime
+        assimilate = self._nsargs.assimilate
+        reinstall = self._nsargs.reinstall
+        verbose = self._nsargs.verbose
+        quiet = self._nsargs.quiet
+        ask = self._nsargs.ask
+        pretend = self._nsargs.pretend
+        fetch = self._nsargs.fetch
+
+        if not quiet:
+            entropy_client.output(
+                "%s..." % (
+                    blue(_("Checking system files")),),
+                header=darkred(" @@ "))
+
+        inst_repo = entropy_client.installed_repository()
+        with inst_repo.shared():
+            faulty_pkg_ids = self._oscheck_scan_unlocked(
+                entropy_client, inst_repo, quiet, verbose, assimilate)
+
         if not faulty_pkg_ids:
             if not quiet:
                 entropy_client.output(
@@ -506,10 +517,12 @@ System security tools.
         """
         Solo Security Update command.
         """
+        # sync() uses wait_resources(), so no locking needed here
         sec = entropy_client.Security()
         return sec.sync(force=self._nsargs.force)
 
-    def _list(self, entropy_client):
+    @sharedlock
+    def _list(self, entropy_client, _inst_repo):
         """
         Solo Security List command.
         """
@@ -557,7 +570,8 @@ System security tools.
                     entropy_client.output(description)
         return 0
 
-    def _info(self, entropy_client):
+    @sharedlock
+    def _info(self, entropy_client, _inst_repo):
         """
         Solo Security Info command.
         """
@@ -593,25 +607,28 @@ System security tools.
         fetch = self._nsargs.fetch
 
         sec = entropy_client.Security()
-        inst_repo = entropy_client.installed_repository()
 
         entropy_client.output(
             "%s..."  % (
                 blue(_("Calculating security updates")),),
             header=darkred(" @@ "))
 
-        affected_atoms = sec.get_affected_packages()
+        inst_repo = entropy_client.installed_repository()
+        with inst_repo.shared():
 
-        valid_matches = set()
-        for atom in affected_atoms:
-            inst_package_id, pkg_rc = inst_repo.atomMatch(atom)
-            if pkg_rc != 0:
-                continue
+            affected_atoms = sec.get_affected_packages()
 
-            key_slot = inst_repo.retrieveKeySlotAggregated(inst_package_id)
-            package_id, repository_id = entropy_client.atom_match(key_slot)
-            if package_id != -1:
-                valid_matches.add((package_id, repository_id))
+            valid_matches = set()
+            for atom in affected_atoms:
+                inst_package_id, pkg_rc = inst_repo.atomMatch(atom)
+                if pkg_rc != 0:
+                    continue
+
+                key_slot = inst_repo.retrieveKeySlotAggregated(
+                    inst_package_id)
+                package_id, repository_id = entropy_client.atom_match(key_slot)
+                if package_id != -1:
+                    valid_matches.add((package_id, repository_id))
 
         if not valid_matches:
             entropy_client.output(
