b4f445a61b0b7ab7e11ab392792ea3fd724bccea
Fabio Erculiani <lxnay@sabayon.org>
2013-12-24 06:32:47 +0100

    [entropy.client.repository] drop entropy_updates_alert argument
    This is no longer useful for the implemented use cases

--- lib/entropy/client/interfaces/repository.py
+++ lib/entropy/client/interfaces/repository.py
@@ -62,8 +62,7 @@ class Repository(object):
     """
 
     def __init__(self, entropy_client, repo_identifiers = None,
-        force = False, entropy_updates_alert = True, fetch_security = True,
-        gpg = True):
+        force = False, fetch_security = True, gpg = True):
         """
         Entropy Client Repositories management interface constructor.
 
@@ -89,7 +88,6 @@ class Repository(object):
         self.need_packages_cleanup = False
         self.updated_repos = set()
         self.fetch_security = fetch_security
-        self.entropy_updates_alert = entropy_updates_alert
         self.already_updated = 0
         self.not_available = 0
         self._gpg_feature = gpg
@@ -202,9 +200,6 @@ class Repository(object):
             self.sync_errors = True
             return 128
 
-        if self.entropy_updates_alert:
-            self._check_entropy_updates()
-
         if self.updated:
             pkgs = self._entropy.clean_downloaded_packages(dry_run = True)
             number_of_pkgs = len(pkgs)
@@ -248,28 +243,6 @@ class Repository(object):
                         )
 
         return 0
-
-    def _check_entropy_updates(self):
-        rc = False
-        if self.entropy_updates_alert:
-            try:
-                rc, pkg_match = self._entropy.check_package_update(
-                    "sys-apps/entropy", deep = True)
-            except:
-                pass
-        if rc:
-            self.new_entropy = True
-            mytxt = "%s: %s. %s." % (
-                bold("Entropy"),
-                blue(_("a new release is available")),
-                darkred(_("Mind to install it before any other package")),
-            )
-            self._entropy.output(
-                mytxt,
-                importance = 1,
-                level = "info",
-                header = bold(" !!! ")
-            )
 
     def _set_last_successful_sync_time(self):
         """
