df6d28f58f76085357278e1fa40253e600c898b6
Fabio Erculiani <lxnay@sabayon.org>
2013-12-27 18:50:41 +0100

    [entropy.client.db] stop using EntropyCacher.CACHE_IDS['mask_filter']
    Use atomMatchCacheKey() instead

--- lib/entropy/client/interfaces/db.py
+++ lib/entropy/client/interfaces/db.py
@@ -2234,7 +2234,6 @@ class MaskableRepository(EntropyRepositoryBase):
     The only repositories that need to support the feature are those containing
     installable packages, like AvailablePackagesRepository.
     """
-    _MASK_FILTER_CACHE_ID = EntropyCacher.CACHE_IDS['mask_filter']
 
     _real_client_settings = None
     _real_client_settings_lock = threading.Lock()
@@ -2256,16 +2255,33 @@ class MaskableRepository(EntropyRepositoryBase):
 
         return self._real_client_settings
 
+    @property
+    def _settings_client_plugin(self):
+        """
+        Get the ClientSystemSettingsPlugin instance from Entropy Client.
+        """
+        from entropy.client.interfaces import Client
+        return Client()._settings_client_plugin
+
     def _mask_filter_fetch_cache(self, package_id):
         if self._caching:
-            return loadobj("%s/%s/%s" % (
-                MaskableRepository._MASK_FILTER_CACHE_ID, self.name,
-                    package_id,))
+            return loadobj(
+                "MaskableRepositoryFilter/%s_%s/%s" % (
+                    self.name,
+                    self.atomMatchCacheKey(),
+                    package_id,
+                    )
+                )
 
     def _mask_filter_store_cache(self, package_id, value):
         if self._caching:
-            dumpobj("%s/%s/%s" % (MaskableRepository._MASK_FILTER_CACHE_ID,
-                self.name, package_id,), value)
+            dumpobj(
+                "MaskableRepositoryFilter/%s_%s/%s" % (
+                    self.name,
+                    self.atomMatchCacheKey(),
+                    package_id,
+                    ),
+                value)
 
     def _maskFilter_live(self, package_id):
 
@@ -2673,6 +2689,15 @@ class MaskableRepository(EntropyRepositoryBase):
         validator_cache[(package_id, self.name, live)] = -1, myr
         self._mask_filter_store_cache(package_id, data)
         return -1, myr
+
+    def atomMatchCacheKey(self):
+        """
+        Reimplemented from EntropyRepositoryBase.
+        """
+        return "%s_%s" % (
+            SystemSettings().packages_configuration_hash(),
+            self._settings_client_plugin.packages_configuration_hash(),
+            )
 
 
 class AvailablePackagesRepository(CachedRepository, MaskableRepository):
