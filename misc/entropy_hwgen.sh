87702f9aa04c550d1b914869d17f16724b4037c9
Fabio Erculiani <lxnay@sabayon.org>
2014-05-05 18:35:48 +0100

    [misc] update ifconfig and sha256sum usage in hwgen

--- misc/entropy_hwgen.sh
+++ misc/entropy_hwgen.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 # Entropy Hardware hash generator
 
-[ -x "/sbin/ifconfig" ] || exit 1
 [ -x "/usr/bin/awk" ] || exit 1
 [ -x "/usr/sbin/lspci" ] || exit 1
-[ -x "/usr/bin/sha256sum" ] || exit 1
 
-ifconfig_output=$(/sbin/ifconfig -a | grep HWaddr | awk '{ print $5 }' 2> /dev/null)
+ifconfig_output=$(ifconfig -a | grep HWaddr | awk '{ print $5 }' 2> /dev/null)
 lspci_output=$(/usr/sbin/lspci -n | cut -d" " -f 3- 2> /dev/null)
-echo $ifconfig_output$lspci_output | /usr/bin/sha256sum | cut -d" " -f 1
+echo $ifconfig_output$lspci_output | sha256sum | cut -d" " -f 1 2> /dev/null
