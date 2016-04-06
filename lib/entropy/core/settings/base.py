Sabayon/entropy

commit a2315b1c92504d85c6f3a9cd7e66088823bc0f2f

[entropy.core] _system_parser: drop access() usage

lib/entropy/core/settings/base.py
@@ -1606,11 +1606,13 @@ class SystemSettings(Singleton, EntropyPluginStore):
             'spm_backend': None,
         }
 
-        if not (os.path.isfile(etp_conf) and \
-            os.access(etp_conf, os.R_OK)):
-            cache_obj['data'] = data
-            self.__mtime_cache[cache_key] = cache_obj
-            return data
+        try:
+            with open(etp_conf, "r") as etp_r:
+                cache_obj['data'] = data
+                self.__mtime_cache[cache_key] = cache_obj
+                return data
+        except (OSError, IOError):
+            pass
 
         const_secure_config_file(etp_conf)
         enc = etpConst['conf_encoding']

commit c530299fb5084367fde35953bbe27e55bf120a83

[entropy.core] _system_parser: Actually read the file
Fixed bug introduced in: a2315b1 [entropy.core] _system_parser: drop access() usage

This ensures that /etc/entropy/entropy.conf is read, instead of being
ignored.

lib/entropy/core/settings/base.py
@@ -1611,7 +1611,7 @@ class SystemSettings(Singleton, EntropyPluginStore):
             'spm_backend': None,
         }
 
-        if const_file_readable(etp_conf):
+        if not const_file_readable(etp_conf):
             return data
 
         const_secure_config_file(etp_conf)