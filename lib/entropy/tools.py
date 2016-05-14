67a645fabe4dcc99a63c7999eaf989cfbc11de51
mudler <mudler@sabayon.org>
2016-05-14 11:49:37 +0200

    [entropy.tools] add IA_64 to elf_class_strtoint

--- lib/entropy/tools.py
+++ lib/entropy/tools.py
@@ -2610,7 +2610,7 @@ def elf_class_strtoint(elf_class_str):
     @return: ELF class int value
     @rtype: int
     """
-    if elf_class_str in ("X86_64", "ELFCLASS64", "PARISC", "SPARCV9", "AARCH64", "PPC64"):
+    if elf_class_str in ("X86_64", "ELFCLASS64", "PARISC", "SPARCV9", "AARCH64", "PPC64", "IA_64"):
         return 2
     elif elf_class_str in ("ARM", "386", "ELFCLASS32", "SPARC", "MIPS","PPC"):
         return 1
